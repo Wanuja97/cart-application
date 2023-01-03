3cea31ac5ed519df109a0b6764c96b31321829b2
wanuja97 <wanuja18@gmailcom>
2023-01-03 22:40:01 +0530

    Product table and seeds created

---
+++ database/seeders/CreateProductSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+class CreateProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            [
+               'product_name'=>'Anchor Milk Powder',
+               'description'=>'400g',
+               'price'=>'1350',
+               'available_qty'=>'100',
+            ],
+            [
+                'product_name'=>'Coca Cola',
+                'description'=>'1.5l',
+                'price'=>'500',
+                'available_qty'=>'200',
+            ],
+        ];
+  
+        foreach ($user as $key => $value) {
+            Product::create($value);
+        }
+    }
+}
