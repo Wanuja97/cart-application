8dd7b7f43a732b49e7b5283a474a0d2c44771ac0
wanuja97 <wanuja18@gmailcom>
2023-01-05 15:09:46 +0530

    admin routes protected

--- routes/web.php
+++ routes/web.php
@@ -31,13 +31,29 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin/dashboard', [HomeController::class, 'adminDashboard'])->name('admin.adminDashboard')->middleware('is_admin');
 
-//Admin Product
-Route::get('admin/products/all',[ProductController::class,'allProducts'])->name('view.products');
-Route::get('admin/product/add',[ProductController::class,'add'])->name('add.product');
-Route::post('admin/product/create',[ProductController::class,'create'])->name('create.product');
-Route::get('admin/product/delete/{id}',[ProductController::class,'delete'])->name('delete.product');
-Route::get('admin/product/edit/{id}',[ProductController::class,'edit']);
-Route::post('admin/product/update/{id}',[ProductController::class,'update']);
+
+
+// Public routes
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+
+
+//  Admin routes
+Route::middleware(['is_admin'])->group(function () {
+
+    // Dashboard routes
+    Route::get('/admin/dashboard', [HomeController::class, 'adminDashboard'])->name('admin.adminDashboard');
+
+    // product routes
+    Route::get('admin/products/all', [ProductController::class, 'allProducts'])->name('view.products');
+    Route::get('admin/product/add', [ProductController::class, 'add'])->name('add.product');
+    Route::post('admin/product/create', [ProductController::class, 'create'])->name('create.product');
+    Route::get('admin/product/delete/{id}', [ProductController::class, 'delete'])->name('delete.product');
+    Route::get('admin/product/edit/{id}', [ProductController::class, 'edit']);
+    Route::post('admin/product/update/{id}', [ProductController::class, 'update']);
+});
