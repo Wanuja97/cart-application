0abfb6aaca678e871b09b9e10688b2c3b4559a1e
wanuja97 <wanuja18@gmailcom>
2023-01-09 18:28:00 +0530

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\Auth;
