32c5826eb5598471521f4e72e69010789bc6b4a4
wanuja97 <wanuja18@gmailcom>
2023-01-20 06:59:59 +0530

    admin dashboard layout updated

--- resources/views/admin/adminDashboard.blade.php
+++ resources/views/admin/adminDashboard.blade.php
@@ -2,42 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
                 <div class="card-body">
-                    <div class="row tiles-section">
-                        <div class="col-sm-4">
-                            <a href="{{route('add.product')}}">
-                                <div class="tile purple">
-                                    <h3 class="title">Add New Product</h3>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-4">
-                            <a href="{{route('view.products')}}">
-                                <div class="tile red">
-                                    <h3 class="title">View Products</h3>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-4">
-                            <a href="{{route('view.consumers')}}">
-                                <div class="tile orange">
-                                    <h3 class="title">View Customers</h3>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <a href="{{route('view.sales')}}">
-                                <div class="tile green">
-                                    <h3 class="title">Sales History</h3>
-                                </div>
-                            </a>
-                        </div>
+                    <div class="col tiles-section">
+                        <a href="{{route('add.product')}}">
+                            <div class="tile purple">
+                                <h3 class="title">Add New Product</h3>
+                            </div>
+                        </a>
+                        <a href="{{route('view.products')}}">
+                            <div class="tile red">
+                                <h3 class="title">View Products</h3>
+                            </div>
+                        </a>
+                        <a href="{{route('view.consumers')}}">
+                            <div class="tile orange">
+                                <h3 class="title">View Customers</h3>
+                            </div>
+                        </a>
+                        <a href="{{route('view.sales')}}">
+                            <div class="tile green">
+                                <h3 class="title">Sales History</h3>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
